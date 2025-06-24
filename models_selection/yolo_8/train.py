46e80fefc57846998ffd17df156780586ddbb71c
firsovmikhael <35039702+torpedonosec80@users.noreply.github.com>
2025-06-24 20:08:46 +0300

    Create train.py

---
+++ models_selection/yolo_8/train.py
@@ -0,0 +1,21 @@
+from ultralytics import YOLO
+import os
+
+def run_train_model(model_weights, YOLO_DATA_DIR, NUM_EPOCHS, BATCH_SIZE):
+  # Инициализация модели. Используем детекционную модель
+  model = YOLO(model_weights) 
+    
+  # Параметры обучения
+  train_params = {
+      'data': os.path.join(YOLO_DATA_DIR, 'dataset.yaml'),
+      'epochs': NUM_EPOCHS,
+      'batch': BATCH_SIZE,
+      'imgsz': 640,
+      'device': '0' if torch.cuda.is_available() else 'cpu',
+      'workers': 2,
+      'name': 'yolo_object_detection'
+  }
+  
+  # Обучение модели
+  results = model.train(**train_params)
+  return results
