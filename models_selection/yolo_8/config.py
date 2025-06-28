d29627aab7daa38b01ef0ac6857a809bc51d0225
firsovmikhael <35039702+torpedonosec80@users.noreply.github.com>
2025-06-28 20:41:43 +0300

    Update config.py

--- models_selection/yolo_8/config.py
+++ models_selection/yolo_8/config.py
@@ -3,12 +3,11 @@ CLASSES = {
     0: 'background',
     1: 'shta-9m',
     2: 'shta-ps',
-    3: 'unknown',
-    4: 'shta-7m',
-    5: 'e2-6u',
-    6: 'shta-9',
-    7: 'shta-3',
-    8: 'sv-5'
+    3: 'shta-7m',
+    4: 'e2-6u',
+    5: 'shta-9',
+    6: 'shta-3',
+    7: 'sv-5'
 }
 CLASS_NAME_TO_ID = {v: k for k, v in CLASSES.items()}
 NUM_CLASSES = len(CLASSES)
